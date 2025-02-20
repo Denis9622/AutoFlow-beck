fb23039e910cd61a3fd1cf31d7c9578b792cdb16
Denis9622 <deniszinchenko96@gmail.com>
2025-02-21 00:44:26 +0200

    upcc

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: "https://auto-flow-front.vercel.app",
   credentials: true,
 };
 
