70e7e0e225ffe57da435a0c6343459e1f8af09bb
Denis9622 <deniszinchenko96@gmail.com>
2025-02-21 00:09:49 +0200

    up

--- src/server.js
+++ src/server.js
@@ -16,9 +16,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 };
+
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
